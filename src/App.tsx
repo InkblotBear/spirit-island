1877091e6814aa9c83e4dd90cb2a64c15cb4d276
JosephHicks93 <josephhicks@gmx.com>
2022-11-08 17:26:15 -0800

    Display board information

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {phases, boardA, TerrainTypes} from './board';
+import {phases, boardA, TerrainTypes, Tile} from './board';
 
 function App() {
   console.log('before');
   console.log(boardA);
-  phases.invader.invaderActions.build(boardA, [TerrainTypes.MOUNTAIN]);
+  // phases.invader.invaderActions.build(boardA, [TerrainTypes.MOUNTAIN]);
   console.log('after');
   console.log(boardA);
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          beeeeeeeans.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.values(boardA).map((tile: Tile) => {
+        return <div>
+          <div>Id: {tile.id}</div>
+          <div>Terrain: {tile.terrain}</div>
+          <div>Pieces: {tile.pieces.join(', ')}</div>
+          <div>Touching: {tile.touching.join(', ')}</div>
+          <br/>
+        </div>
+      })}
     </div>
   );
 }
