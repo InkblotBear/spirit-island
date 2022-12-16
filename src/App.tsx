f221f41207a4f5ee3dab1b4d7837698d0889f47f
Silk Neon <ksiondag846@gmail.com>
2022-12-15 21:07:07 -0500

    Adding immer, start of invader action deck

--- src/App.tsx
+++ src/App.tsx
@@ -2,102 +2,30 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {phases, createBoardA, TerrainTypes, Tile, boardAAsGrid} from './board';
+import {phases, createBoardA, TerrainTypes, Tile, boardAAsGrid, Phases, makeTheInvaderDeck} from './board';
 import { Col, Dropdown, MenuProps, Row } from 'antd';
 import TileView from './components/TileView';
 
+import produce from "immer";
 
 function App() {
   const [board, setBoard] = useState(createBoardA());
-  const [buildType, setBuildType] = useState(TerrainTypes.MOUNTAIN);
-  const [exploreType, setExploreType] = useState(TerrainTypes.MOUNTAIN);
-  const [ravageType, setRavageType] = useState(TerrainTypes.MOUNTAIN);
+  const [phase, setPhase] = useState(Phases.invaderRavage);
+  const [invaderDeck, setInvaderDeck] = useState(makeTheInvaderDeck());
 
-  const handleBuildClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('click left button', e);
-    phases.invader.invaderActions.build(board, [buildType]);
-    setBoard({...board});
-  };
-  
-  const handleBuildTypeClick: MenuProps['onClick'] = e => {
-    console.log('click', e);
-    setBuildType(e.key as TerrainTypes);
-  };
-
-  const handleExploreClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('click left button', e);
-    phases.invader.invaderActions.explore(board, [exploreType]);
-    setBoard({...board});
-  };
-
-  const handleRavageClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('click left button', e);
-    phases.invader.invaderActions.ravage(board, [ravageType]);
-    setBoard({...board});
-  };
-  
-  const handleExploreTypeClick: MenuProps['onClick'] = e => {
-    console.log('click', e);
-    setExploreType(e.key as TerrainTypes);
-  };
-
-  const handleRavageTypeClick: MenuProps['onClick'] = e => {
-    console.log('click', e);
-    setRavageType(e.key as TerrainTypes);
-  };
-
-  const items: MenuProps['items'] = [
-    {
-      label: TerrainTypes.COASTAL,
-      key: TerrainTypes.COASTAL,
-    },
-    {
-      label: TerrainTypes.DESERT,
-      key: TerrainTypes.DESERT,
-    },
-    {
-      label: TerrainTypes.JUNGLE,
-      key: TerrainTypes.JUNGLE,
-    },
-    {
-      label: TerrainTypes.MOUNTAIN,
-      key: TerrainTypes.MOUNTAIN,
-    },
-    {
-      label: TerrainTypes.WETLAND,
-      key: TerrainTypes.WETLAND,
-    },
-  ];
-  
-  const buildProps = {
-    items,
-    onClick: handleBuildTypeClick,
-  };
-
-  const exploreProps = {
-    items,
-    onClick: handleExploreTypeClick,
-  };
-
-  const ravageProps = {
-    items,
-    onClick: handleRavageTypeClick,
-  };
   
   return (
     <div className="App">
-      <Dropdown.Button menu={buildProps} onClick={handleBuildClick}>
-        Build {buildType}
-      </Dropdown.Button>
-
-      <Dropdown.Button menu={exploreProps} onClick={handleExploreClick}>
-        Explore {exploreType}
-      </Dropdown.Button>
-
-      <Dropdown.Button menu={ravageProps} onClick={handleRavageClick}>
-        Ravage {ravageType}
-      </Dropdown.Button>
-
+      <div>
+          Ravage: [{invaderDeck.ravage?.join(', ')}],
+          Build: [{invaderDeck.build?.join(', ')}],
+          Explore: [{invaderDeck.explore?.join(', ')}]
+      </div>
+      <div onClick={() => {
+        setInvaderDeck(produce((invaderDeck) => {
+          invaderDeck.advanceInvaderCards();
+        }));
+      }}>Advance the Invader Deck</div>
       {boardAAsGrid.map((row, rowIndex) => {
         return <Row key={rowIndex} gutter={[16, 16]}>
           {row.map((id, colIndex) => {
