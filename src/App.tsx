c960865b2a6190ac02fadc39e61e26d6179df5e8
Silk Neon <ksiondag846@gmail.com>
2023-01-14 10:29:19 -0800

    Installing basic prettier setup
    Running prettier on all files
    
    Adding husky (precommit stuffs)

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,131 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import {phases, createBoardA, TerrainTypes, Tile, boardAAsGrid, Phases, makeTheInvaderDeck} from './board';
-import { Col, Dropdown, MenuProps, Row } from 'antd';
-import TileView from './components/TileView';
+import {
+  phases,
+  createBoardA,
+  TerrainTypes,
+  Tile,
+  boardAAsGrid,
+  Phases,
+  makeTheInvaderDeck,
+} from "./board";
+import { Col, Dropdown, MenuProps, Row } from "antd";
+import TileView from "./components/TileView";
 
 import produce from "immer";
-import SpiritBoard from './SpiritBoard';
-import { useSelector } from 'react-redux';
+import SpiritBoard from "./SpiritBoard";
+import { useSelector } from "react-redux";
 
-import { IRootState } from './store';
-import { advanceInvaderDeck } from './features/boardA';
+import { IRootState } from "./store";
+import { advanceInvaderDeck } from "./features/boardA";
 
 function App() {
-  const board = useSelector<IRootState, IRootState['board']>(state => state.board);
+  const board = useSelector<IRootState, IRootState["board"]>(
+    (state) => state.board
+  );
   const [phase, setPhase] = useState(Phases.invaderRavage);
   const [invaderDeck, setInvaderDeck] = useState(makeTheInvaderDeck());
-  
+
   return (
     <div className="App">
       <div>
-          Ravage: [{invaderDeck.ravage?.join(', ')}],
-          Build: [{invaderDeck.build?.join(', ')}],
-          {/* TODO: Explore shouldn't be revealed until explore phase starts */}
-          Explore: [{invaderDeck.explore?.join(', ')}]
+        Ravage: [{invaderDeck.ravage?.join(", ")}], Build: [
+        {invaderDeck.build?.join(", ")}],
+        {/* TODO: Explore shouldn't be revealed until explore phase starts */}
+        Explore: [{invaderDeck.explore?.join(", ")}]
       </div>
-      <div onClick={() => {
-        advanceInvaderDeck(invaderDeck);
-        setInvaderDeck(invaderDeck.advanceInvaderCards());
-      }}>Advance the Invader Deck</div>
+      <div
+        onClick={() => {
+          advanceInvaderDeck(invaderDeck);
+          setInvaderDeck(invaderDeck.advanceInvaderCards());
+        }}
+      >
+        Advance the Invader Deck
+      </div>
       {boardAAsGrid.map((row, rowIndex) => {
-        return <Row key={rowIndex} gutter={[16, 16]}>
-          {row.map((id, colIndex) => {
-            const style = {} as any;
-            let renderState = true;
-            if (rowIndex === 0 || boardAAsGrid[rowIndex-1][colIndex] !== id) {
-              style['border-top'] = 'solid';
-            } 
-            if (rowIndex === boardAAsGrid.length - 1 || boardAAsGrid[rowIndex+1][colIndex] !== id) {
-              style['border-bottom'] = 'solid';
-            }
-            if (colIndex === 0 || boardAAsGrid[rowIndex][colIndex-1] !== id) {
-              style['border-left'] = 'solid';
+        return (
+          <Row key={rowIndex} gutter={[16, 16]}>
+            {row.map((id, colIndex) => {
+              const style = {} as any;
+              let renderState = true;
+              if (
+                rowIndex === 0 ||
+                boardAAsGrid[rowIndex - 1][colIndex] !== id
+              ) {
+                style["border-top"] = "solid";
+              }
+              if (
+                rowIndex === boardAAsGrid.length - 1 ||
+                boardAAsGrid[rowIndex + 1][colIndex] !== id
+              ) {
+                style["border-bottom"] = "solid";
+              }
+              if (
+                colIndex === 0 ||
+                boardAAsGrid[rowIndex][colIndex - 1] !== id
+              ) {
+                style["border-left"] = "solid";
+              }
+              if (
+                colIndex === boardAAsGrid[rowIndex].length - 1 ||
+                boardAAsGrid[rowIndex][colIndex + 1] !== id
+              ) {
+                style["border-right"] = "solid";
+              }
 
-            }
-            if (colIndex === boardAAsGrid[rowIndex].length - 1 || boardAAsGrid[rowIndex][colIndex+1] !== id) {
-              style['border-right'] = 'solid';
-            }
+              if (
+                rowIndex !== 0 &&
+                boardAAsGrid[rowIndex - 1][colIndex] === id
+              ) {
+                renderState = false;
+              }
+              if (
+                colIndex !== 0 &&
+                boardAAsGrid[rowIndex][colIndex - 1] === id
+              ) {
+                renderState = false;
+              }
 
-            if (rowIndex !== 0 && boardAAsGrid[rowIndex-1][colIndex] === id) {
-              renderState = false;
-            }
-            if (colIndex !== 0 && boardAAsGrid[rowIndex][colIndex-1] === id) {
-              renderState = false;
-            }
-
-
-            const tile = board[id];
-            if (tile !== undefined) {
-              return <Col style={style} key={colIndex} span={4}>
-                {renderState && (
-                  <div>
-                    <div>Id: {tile.id}</div>
-                    <div>Terrain: {tile.terrain}</div>
-                    <div>Pieces: {tile.pieces.map((piece) => typeof piece === "string" ? piece : piece.type).join(', ')}</div>
-                  </div>
-                )}
-              </Col>  
-            }          
-            return <Col style={style} key={colIndex} span={4}>
-              {renderState && (
-                <div>
-                  <div>Id: 0</div>
-                  <div>Terrain: Ocean</div>
-                  <div>Pieces: </div>
-                </div>
-              )}
-            </Col>
-          })}
-        </Row>
+              const tile = board[id];
+              if (tile !== undefined) {
+                return (
+                  <Col style={style} key={colIndex} span={4}>
+                    {renderState && (
+                      <div>
+                        <div>Id: {tile.id}</div>
+                        <div>Terrain: {tile.terrain}</div>
+                        <div>
+                          Pieces:{" "}
+                          {tile.pieces
+                            .map((piece) =>
+                              typeof piece === "string" ? piece : piece.type
+                            )
+                            .join(", ")}
+                        </div>
+                      </div>
+                    )}
+                  </Col>
+                );
+              }
+              return (
+                <Col style={style} key={colIndex} span={4}>
+                  {renderState && (
+                    <div>
+                      <div>Id: 0</div>
+                      <div>Terrain: Ocean</div>
+                      <div>Pieces: </div>
+                    </div>
+                  )}
+                </Col>
+              );
+            })}
+          </Row>
+        );
       })}
-      <TileView tile={board[1]} setTile={() => null}/>
-      <SpiritBoard/>
+      <TileView tile={board[1]} setTile={() => null} />
+      <SpiritBoard />
     </div>
   );
 }
--- src/components/TileView/TileView.tsx
+++ src/components/TileView/TileView.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { Tile } from '../../board';
+import React from "react";
+import { Tile } from "../../board";
 
 export interface TileViewProps {
-    damage?: {
-        targets?: unknown[];
-        amount: number;
-    };
-    tile: Tile | undefined;
-    setTile: (tile: Tile) => void;
+  damage?: {
+    targets?: unknown[];
+    amount: number;
+  };
+  tile: Tile | undefined;
+  setTile: (tile: Tile) => void;
 }
 
-function TileView ({tile, setTile}: TileViewProps) {
-    return <div>
-        {tile?.pieces.map((piece) => {
-            if (typeof piece === "string") {
-                return <div>{piece}</div>;
-            }
-            return <div>{piece.type} {piece.currentHealth}/{piece.baseHealth} Damage: {piece.damage}</div>
-        })}
-    </div>;
+function TileView({ tile, setTile }: TileViewProps) {
+  return (
+    <div>
+      {tile?.pieces.map((piece) => {
+        if (typeof piece === "string") {
+          return <div>{piece}</div>;
+        }
+        return (
+          <div>
+            {piece.type} {piece.currentHealth}/{piece.baseHealth} Damage:{" "}
+            {piece.damage}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default TileView;
